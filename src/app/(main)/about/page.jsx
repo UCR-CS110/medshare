374d2832e67afd8dd7052169d56689ff4422a6d2
Keanu Wilson <wolffox030701@gmail.com>
2026-06-11 16:40:02 -0700

    Basic headers in about page created

--- src/app/(main)/about/page.jsx
+++ src/app/(main)/about/page.jsx
@@ -3,8 +3,12 @@
 function About() {
     return (
         <div>
-            <h1>About Page</h1>
-            {/* About content will go here */}
+            <h2 className="text-2xl font-bold">Our Mission</h2>
+            {/* Mission content will go here */}
+            <h2 className="text-2xl font-bold">How It Works</h2>
+            {/* How it works content will go here */}
+            <h2 className="text-2xl font-bold">About us</h2>
+            {/* About us content will go here */}
         </div>
     );
 }
